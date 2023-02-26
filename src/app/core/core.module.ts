d565de139c3775487fdd9d86a869c1a3357985ce
neverswood <nevers_wood@hotmail.fr>
2023-02-26 23:16:11 +0100

    Import RouterModule in the CoreModule for use routerLink

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
