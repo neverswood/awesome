7edfaf53cbaa13097d4bf09867fbcaae617292b0
neverswood <nevers_wood@hotmail.fr>
2023-03-09 22:51:46 +0100

    Use of the router for social-media

--- src/app/social-media/social-media-routing.module.ts
+++ src/app/social-media/social-media-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: PostListComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SocialMediaRoutingModule { }
+export class SocialMediaRoutingModule {}
